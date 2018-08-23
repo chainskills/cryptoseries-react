9fde439cb2977a87ef7bb22dd891965365f07728
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2018-08-24 01:44:12 +0900

    Added a simple function to check the number of blocks the owner has to wait before he can publish again.
    Implemented the full state visibility features.

--- contracts/Series.sol
+++ contracts/Series.sol
@@ -4,203 +4,211 @@ import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract Series is Ownable {
-  using SafeMath for uint;
-  
-  //title of the series
-  string public title;
-  //amount that the owner will receive from each pledger for each episode
-  uint public pledgePerEpisode;
-  //periodicity of episodes, in number of blocks
-  //to limit the possibility of owner to drain pledgers' accounts by publishing multiple episodes quickly
-  uint public minimumPublicationPeriod;
+    using SafeMath for uint;
 
-  //to keep track of how much each publisher pledged, knowing that this pledge will decrease each time a new episode is published, or when a pledger withdraws everything from his account
-  mapping(address => uint) public pledges;
-  //to keep a list of all the pledgers into the series
-  address[] pledgers;
-  //to keep track of the last time an episode was published, and limit the frequency of publications
-  uint public lastPublicationBlock;
-  //to keep track of all published episodes
-  mapping(uint => string) public publishedEpisodes;
-  uint public episodeCounter;
+    //title of the series
+    string public title;
+    //amount that the owner will receive from each pledger for each episode
+    uint public pledgePerEpisode;
+    //periodicity of episodes, in number of blocks
+    //to limit the possibility of owner to drain pledgers' accounts by publishing multiple episodes quickly
+    uint public minimumPublicationPeriod;
 
-  //events
-  /*
-  * Emitted when a new pledger joins the series
-  * @param pledger Address of the new pledger
-  */
-  event NewPledger(address pledger);
-  /*
-  * Emitted when a pledger pledges some amount, whether he is a new pledger or a previous one just topping up his account
-  * @param pledger Address of the pledger
-  * @param pledge Amount pledged this time
-  * @param totalPledge Balance of this pledger's account including what he just pledged
-  */
-  event NewPledge(address indexed pledger, uint pledge, uint totalPledge);
-  /*
-  * Emitted when the owner published a new episode
-  * @param episodeHash Hash of the published episode
-  * @param episodePay How much the owner received for this episode
-  */
-  event NewPublication(uint episodeId, string episodeLink, uint episodePay);
-  /*
-  * Emitted when a pledger withdraws all his pledge from his account
-  * @param pledger Address of the withdrawing pledger
-  * @param pledge Amount that was sent back to the pledger
-  */
-  event Withdrawal(address indexed pledger, uint pledge);
-  /*
-  * Emitted when a pledge goes lower than pledgePerEpisode, that can be used to notify the pledger that he should top up his account before next episode is published
-  * @param pledger Address of the pledger
-  * @param pledge Current balance of the pledger
-  */
-  event PledgeInsufficient(address indexed pledger, uint pledge);
-  /*
-  * Emitted when the show is closed by the owner
-  * @param balanceBeforeClose Balance of the contract at the moment of closing
-  */
-  event SeriesClosed(uint balanceBeforeClose);
+    //to keep track of how much each publisher pledged, knowing that this pledge will decrease each time a new episode is published, or when a pledger withdraws everything from his account
+    mapping(address => uint) public pledges;
+    //to keep a list of all the pledgers into the series
+    address[] pledgers;
+    //to keep track of the last time an episode was published, and limit the frequency of publications
+    uint public lastPublicationBlock;
+    //to keep track of all published episodes
+    mapping(uint => string) public publishedEpisodes;
+    uint public episodeCounter;
 
-  /*
-  * Configures the series parameters
-  * @param title Title of the series
-  * @param pledgePerEpisode Amount the owner will receive for each episode from each pledger
-  * @param minimumPublicationPeriod Number of blocks the owner will have to wait between 2 publications
-  */
-  constructor(string _title, uint _pledgePerEpisode, uint _minimumPublicationPeriod) public {
-    title = _title;
-    pledgePerEpisode = _pledgePerEpisode;
-    minimumPublicationPeriod = _minimumPublicationPeriod;
-  }
+    //events
+    /*
+    * Emitted when a new pledger joins the series
+    * @param pledger Address of the new pledger
+    */
+    event NewPledger(address pledger);
+    /*
+    * Emitted when a pledger pledges some amount, whether he is a new pledger or a previous one just topping up his account
+    * @param pledger Address of the pledger
+    * @param pledge Amount pledged this time
+    * @param totalPledge Balance of this pledger's account including what he just pledged
+    */
+    event NewPledge(address indexed pledger, uint pledge, uint totalPledge);
+    /*
+    * Emitted when the owner published a new episode
+    * @param episodeHash Hash of the published episode
+    * @param episodePay How much the owner received for this episode
+    */
+    event NewPublication(uint episodeId, string episodeLink, uint episodePay);
+    /*
+    * Emitted when a pledger withdraws all his pledge from his account
+    * @param pledger Address of the withdrawing pledger
+    * @param pledge Amount that was sent back to the pledger
+    */
+    event Withdrawal(address indexed pledger, uint pledge);
+    /*
+    * Emitted when a pledge goes lower than pledgePerEpisode, that can be used to notify the pledger that he should top up his account before next episode is published
+    * @param pledger Address of the pledger
+    * @param pledge Current balance of the pledger
+    */
+    event PledgeInsufficient(address indexed pledger, uint pledge);
+    /*
+    * Emitted when the show is closed by the owner
+    * @param balanceBeforeClose Balance of the contract at the moment of closing
+    */
+    event SeriesClosed(uint balanceBeforeClose);
 
-  /**
-  * TRANSACTIONAL FUNCTIONS
-  */
-
-  /*
-  * Lets someone increase their pledge.
-  * The first time you pledge, you must pledge at least the minimum pledge per episode.
-  * Then for every new pledge, the amount you already pledged plus the new amount must be at least the minimum pledge per episode
-  * This function is payable so the pledge must be transmitted with msg.value
-  * The owner cannot pledge on its own series.
-  */
-  function pledge() public payable {
-    require(msg.value.add(pledges[msg.sender]) >= pledgePerEpisode, "Pledge must be greater than minimum pledge per episode");
-    require(msg.sender != owner, "Owner cannot pledge on its own series");
-    bool oldPledger = false;
-    for(uint i = 0; i < pledgers.length; i++) {
-      if(pledgers[i] == msg.sender) {
-        oldPledger = true;
-        break;
-      }
+    /*
+    * Configures the series parameters
+    * @param title Title of the series
+    * @param pledgePerEpisode Amount the owner will receive for each episode from each pledger
+    * @param minimumPublicationPeriod Number of blocks the owner will have to wait between 2 publications
+    */
+    constructor(string _title, uint _pledgePerEpisode, uint _minimumPublicationPeriod) public {
+        title = _title;
+        pledgePerEpisode = _pledgePerEpisode;
+        minimumPublicationPeriod = _minimumPublicationPeriod;
     }
-    if(!oldPledger) {
-      pledgers.push(msg.sender);
-      emit NewPledger(msg.sender);
-    }
-    pledges[msg.sender] = pledges[msg.sender].add(msg.value);
-    emit NewPledge(msg.sender, msg.value, pledges[msg.sender]);
-  }
 
-  /*
-  * This function can only be called by the owner.
-  * And it can only be called if at least minimumPublicationPeriod blocks have passed since lastPublicationBlock
-  * If those prerequisites are met, then the owner receives pledgePerEpisode times number of pledgers whose pledge is still greater than pledgePerEpisode
-  * @param episodeLink Link to download the episode
-  */
-  function publish(string episodeLink) public onlyOwner {
-    require(lastPublicationBlock == 0 || block.number > lastPublicationBlock.add(minimumPublicationPeriod), "Owner cannot publish again so soon");
+    /**
+    * TRANSACTIONAL FUNCTIONS
+    */
 
-    lastPublicationBlock = block.number;
-    episodeCounter++;
-    publishedEpisodes[episodeCounter] = episodeLink;
-
-    //update pledges
-    uint episodePay = 0;
-    for(uint i = 0; i < pledgers.length; i++) {
-      if(isActive(pledgers[i])) {
-        pledges[pledgers[i]] = pledges[pledgers[i]].sub(pledgePerEpisode);
-        episodePay = episodePay.add(pledgePerEpisode);
-        if(!isActive(pledgers[i])) {
-          emit PledgeInsufficient(pledgers[i], pledges[pledgers[i]]);
+    /*
+    * Lets someone increase their pledge.
+    * The first time you pledge, you must pledge at least the minimum pledge per episode.
+    * Then for every new pledge, the amount you already pledged plus the new amount must be at least the minimum pledge per episode
+    * This function is payable so the pledge must be transmitted with msg.value
+    * The owner cannot pledge on its own series.
+    */
+    function pledge() public payable {
+        require(msg.value.add(pledges[msg.sender]) >= pledgePerEpisode, "Pledge must be greater than minimum pledge per episode");
+        require(msg.sender != owner, "Owner cannot pledge on its own series");
+        bool oldPledger = false;
+        for (uint i = 0; i < pledgers.length; i++) {
+            if (pledgers[i] == msg.sender) {
+                oldPledger = true;
+                break;
+            }
         }
-      }
+        if (!oldPledger) {
+            pledgers.push(msg.sender);
+            emit NewPledger(msg.sender);
+        }
+        pledges[msg.sender] = pledges[msg.sender].add(msg.value);
+        emit NewPledge(msg.sender, msg.value, pledges[msg.sender]);
     }
 
-    //pay the owner
-    owner.transfer(episodePay);
-    emit NewPublication(episodeCounter, episodeLink, episodePay);
-  }
+    /*
+    * This function can only be called by the owner.
+    * And it can only be called if at least minimumPublicationPeriod blocks have passed since lastPublicationBlock
+    * If those prerequisites are met, then the owner receives pledgePerEpisode times number of pledgers whose pledge is still greater than pledgePerEpisode
+    * @param episodeLink Link to download the episode
+    */
+    function publish(string episodeLink) public onlyOwner {
+        require(lastPublicationBlock == 0 || block.number > lastPublicationBlock.add(minimumPublicationPeriod), "Owner cannot publish again so soon");
 
-  /*
-  * Let a pledger withdraw his entire pledge from his account
-  */
-  function withdraw() public {
-    uint amount = pledges[msg.sender];
-    if(amount > 0) {
-      pledges[msg.sender] = 0;
-      msg.sender.transfer(amount);
-      emit Withdrawal(msg.sender, amount);
-      emit PledgeInsufficient(msg.sender, 0);
-    }
-  }
+        lastPublicationBlock = block.number;
+        episodeCounter++;
+        publishedEpisodes[episodeCounter] = episodeLink;
 
-  /*
-  * Give their money back to all pledgers before killing the contract
-  */
-  function close() public onlyOwner {
-    uint contractBalance = address(this).balance;
-    for(uint i = 0; i < pledgers.length; i++) {
-      uint amount = pledges[pledgers[i]];
-      if(amount > 0) {
-        pledgers[i].transfer(amount);
-      }
-    }
-    emit SeriesClosed(contractBalance);
-    selfdestruct(owner);
-  }
+        //update pledges
+        uint episodePay = 0;
+        for (uint i = 0; i < pledgers.length; i++) {
+            if (isActive(pledgers[i])) {
+                pledges[pledgers[i]] = pledges[pledgers[i]].sub(pledgePerEpisode);
+                episodePay = episodePay.add(pledgePerEpisode);
+                if (!isActive(pledgers[i])) {
+                    emit PledgeInsufficient(pledgers[i], pledges[pledgers[i]]);
+                }
+            }
+        }
 
-  /**
-  * VIEW FUNCTIONS
-  */
-
-  /*
-  * Total number of pledgers, whether they are still active or not,
-  * that is the number of accounts that have pledged at some point
-  */
-  function totalPledgers() public view returns (uint) {
-    return pledgers.length;
-  }
-
-  /*
-  * Number of active pledgers, that is number of pledgers whose pledge is still
-  * greater than minimum pledge per episode
-  */
-  function activePledgers() public view returns (uint) {
-    uint active = 0;
-    for(uint i = 0; i < pledgers.length; i++) {
-      if(isActive(pledgers[i])) {
-        active++;
-      }
-    }
-    return active;
-  }
-
-  /*
-  * Calculate how much the owner will get paid for next episode
-  */
-  function nextEpisodePay() public view returns (uint) {
-    uint episodePay = 0;
-    for(uint i = 0; i < pledgers.length; i++) {
-      if(isActive(pledgers[i])) {
-        episodePay = episodePay.add(pledgePerEpisode);
-      }
+        //pay the owner
+        owner.transfer(episodePay);
+        emit NewPublication(episodeCounter, episodeLink, episodePay);
     }
 
-    return episodePay;
-  }
+    /*
+    * Let a pledger withdraw his entire pledge from his account
+    */
+    function withdraw() public {
+        uint amount = pledges[msg.sender];
+        if (amount > 0) {
+            pledges[msg.sender] = 0;
+            msg.sender.transfer(amount);
+            emit Withdrawal(msg.sender, amount);
+            emit PledgeInsufficient(msg.sender, 0);
+        }
+    }
 
-  function isActive(address pledger) internal view returns (bool) {
-    return pledges[pledger] >= pledgePerEpisode;
-  }
+    /*
+    * Give their money back to all pledgers before killing the contract
+    */
+    function close() public onlyOwner {
+        uint contractBalance = address(this).balance;
+        for (uint i = 0; i < pledgers.length; i++) {
+            uint amount = pledges[pledgers[i]];
+            if (amount > 0) {
+                pledgers[i].transfer(amount);
+            }
+        }
+        emit SeriesClosed(contractBalance);
+        selfdestruct(owner);
+    }
+
+    /**
+    * VIEW FUNCTIONS
+    */
+
+    /*
+    * Total number of pledgers, whether they are still active or not,
+    * that is the number of accounts that have pledged at some point
+    */
+    function totalPledgers() public view returns (uint) {
+        return pledgers.length;
+    }
+
+    /*
+    * Number of active pledgers, that is number of pledgers whose pledge is still
+    * greater than minimum pledge per episode
+    */
+    function activePledgers() public view returns (uint) {
+        uint active = 0;
+        for (uint i = 0; i < pledgers.length; i++) {
+            if (isActive(pledgers[i])) {
+                active++;
+            }
+        }
+        return active;
+    }
+
+    /*
+    * Calculate how much the owner will get paid for next episode
+    */
+    function nextEpisodePay() public view returns (uint) {
+        uint episodePay = 0;
+        for (uint i = 0; i < pledgers.length; i++) {
+            if (isActive(pledgers[i])) {
+                episodePay = episodePay.add(pledgePerEpisode);
+            }
+        }
+
+        return episodePay;
+    }
+
+    function isActive(address pledger) internal view returns (bool) {
+        return pledges[pledger] >= pledgePerEpisode;
+    }
+
+    function numberOfBlocksToWaitBeforeNextPublish() public view returns (uint) {
+        if(lastPublicationBlock == 0 || block.number >= lastPublicationBlock + minimumPublicationPeriod) {
+            return 0;
+        } else {
+            return lastPublicationBlock + minimumPublicationPeriod - block.number;
+        }
+    }
 }
--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {ContractData, ContractForm} from 'drizzle-react-components';
+import {ContractData} from 'drizzle-react-components';
 import PropTypes from 'prop-types';
 
 class Home extends Component {
@@ -14,11 +14,15 @@ class Home extends Component {
 
         this.contracts = context.drizzle.contracts;
         this.seriesContract = this.contracts.Series;
+
         this.publicationPeriodKey = this.seriesContract.methods.minimumPublicationPeriod.cacheCall();
         this.pledgePerEpisodeKey = this.seriesContract.methods.pledgePerEpisode.cacheCall();
         this.activePledgersKey = this.seriesContract.methods.activePledgers.cacheCall();
         this.totalPledgersKey = this.seriesContract.methods.totalPledgers.cacheCall();
         this.nextEpisodePayKey = this.seriesContract.methods.nextEpisodePay.cacheCall();
+        this.yourPledgeKey = this.seriesContract.methods.pledges.cacheCall(this.props.accounts[0]);
+        this.ownerKey = this.seriesContract.methods.owner.cacheCall();
+        this.numberOfBlocksToWaitKey = this.seriesContract.methods.numberOfBlocksToWaitBeforeNextPublish.cacheCall();
     }
 
     componentDidMount() {
@@ -29,37 +33,118 @@ class Home extends Component {
         const seriesState = this.props.Series;
         const loading = <span>Loading...</span>;
 
+        let buttons = loading;
+        let isOwner = false;
+        let withdrawable = false;
+        if (this.ownerKey in seriesState.owner) {
+            const owner = seriesState.owner[this.ownerKey].value;
+            if (owner === this.props.accounts[0]) {
+                isOwner = true;
+            } else {
+                isOwner = false;
+            }
+        }
+
         let minimumPublicationPeriod = loading;
-        if(this.publicationPeriodKey in seriesState.minimumPublicationPeriod) {
+        if (this.publicationPeriodKey in seriesState.minimumPublicationPeriod) {
             const blocks = seriesState.minimumPublicationPeriod[this.publicationPeriodKey].value;
             minimumPublicationPeriod = blocks + " blocks (~" + (blocks * 15 / 3600 / 24) + " days)";
         }
 
         let pledgePerEpisode = loading;
-        if(this.pledgePerEpisodeKey in seriesState.pledgePerEpisode) {
+        if (this.pledgePerEpisodeKey in seriesState.pledgePerEpisode) {
             const pledge = seriesState.pledgePerEpisode[this.pledgePerEpisodeKey].value;
-            pledgePerEpisode = this.web3.utils.fromWei(pledge, "ether") + " ETH";
+            const pledgePerEpisodeEth = this.web3.utils.fromWei(pledge, "ether");
+            pledgePerEpisode = pledgePerEpisodeEth + " ETH";
+            if (this.props.rates && this.props.rates['EUR']) {
+                pledgePerEpisode = pledgePerEpisode + ' (~' + (this.props.rates['EUR'] * pledgePerEpisodeEth).toFixed(2) + '€)';
+            }
         }
 
         let activePledgers = loading;
-        if(this.activePledgersKey in seriesState.activePledgers) {
+        if (this.activePledgersKey in seriesState.activePledgers) {
             activePledgers = seriesState.activePledgers[this.activePledgersKey].value;
         }
 
         let totalPledgers = loading;
-        if(this.totalPledgersKey in seriesState.totalPledgers) {
+        if (this.totalPledgersKey in seriesState.totalPledgers) {
             totalPledgers = seriesState.totalPledgers[this.totalPledgersKey].value;
         }
 
         let nextEpisodePay = loading;
-        if(this.nextEpisodePayKey in seriesState.nextEpisodePay) {
+        if (this.nextEpisodePayKey in seriesState.nextEpisodePay) {
             const nextEpisodePayWei = seriesState.nextEpisodePay[this.nextEpisodePayKey].value;
             const nextEpisodePayEth = this.web3.utils.fromWei(nextEpisodePayWei, "ether");
             nextEpisodePay = nextEpisodePayEth + " ETH";
-            if(this.props.rates && this.props.rates['EUR']) {
-                nextEpisodePay = nextEpisodePay + ' (~' + (this.props.rates['EUR'] * nextEpisodePayEth) + '€)';
+            if (this.props.rates && this.props.rates['EUR']) {
+                nextEpisodePay = nextEpisodePay + ' (~' + (this.props.rates['EUR'] * nextEpisodePayEth).toFixed(2) + '€)';
             }
         }
+
+        let yourPledge = loading;
+        let numberOfEpisodes = loading;
+        if (!isOwner && this.yourPledgeKey in seriesState.pledges) {
+            const yourPledgeWei = seriesState.pledges[this.yourPledgeKey].value;
+            if (yourPledgeWei > 0) {
+                withdrawable = true;
+            }
+            const yourPledgeEth = this.web3.utils.fromWei(yourPledgeWei, "ether");
+            yourPledge = yourPledgeEth + " ETH";
+            if (this.props.rates && this.props.rates['EUR']) {
+                yourPledge = yourPledge + ' (~' + (this.props.rates['EUR'] * yourPledgeEth).toFixed(2) + '€)';
+            }
+
+            if (this.pledgePerEpisodeKey in seriesState.pledgePerEpisode) {
+                const pledgePerEpisodeWei = seriesState.pledgePerEpisode[this.pledgePerEpisodeKey].value;
+                numberOfEpisodes = Math.ceil(yourPledgeWei / pledgePerEpisodeWei);
+                if (numberOfEpisodes === 0) {
+                    numberOfEpisodes = (
+                        <span>
+                        {numberOfEpisodes}
+                            <br/>(you should pledge again if you want to support more episodes)
+                        </span>
+                    );
+                }
+            }
+        }
+
+        let yourSupport = null;
+        if (!isOwner) {
+            yourSupport = (
+                <div>
+                    <h2>Your support</h2>
+                    <p><strong>Your pledge</strong>: {yourPledge}</p>
+                    <p><strong>Number of episodes</strong>: {numberOfEpisodes}</p>
+                </div>
+            );
+        }
+
+        let publishable = false;
+        let numberOfBlocksToWait = loading;
+        if(this.numberOfBlocksToWaitKey in seriesState.numberOfBlocksToWaitBeforeNextPublish) {
+            numberOfBlocksToWait = seriesState.numberOfBlocksToWaitBeforeNextPublish[this.numberOfBlocksToWaitKey].value;
+            publishable = numberOfBlocksToWait == 0;
+        }
+
+        if (isOwner) {
+            buttons = (
+                <span>
+                    <button>Close</button>
+                    &nbsp;
+                    <button disabled={!publishable}>Publish</button>
+                    {!publishable ? "You have to wait for " + numberOfBlocksToWait + " blocks before you can publish again." : null}
+                </span>
+            );
+        } else {
+            buttons = (
+                <span>
+                    <button disabled={!withdrawable}>Withdraw</button>
+                    &nbsp;
+                    <button>Pledge</button>
+                </span>
+            )
+        }
+
         return (
             <main className="container">
                 <div className="pure-g">
@@ -73,7 +158,10 @@ class Home extends Component {
                         <p><strong>Active supporters</strong>: {activePledgers}</p>
                         <p><strong>Followers</strong>: {totalPledgers}</p>
                         <p><strong>Next episode pay</strong>: {nextEpisodePay}</p>
-                        <p>Here we have a form with custom, friendly labels. Also note the token symbol will not display
+                        {yourSupport}
+                        <p>{buttons}</p>
+
+                        {/*<p>Here we have a form with custom, friendly labels. Also note the token symbol will not display
                             a loading indicator. We've suppressed it with the <code>hideIndicator</code> prop because we
                             know this variable is constant.</p>
                         <p><strong>Total Supply</strong>: <ContractData contract="TutorialToken" method="totalSupply"
@@ -85,10 +173,10 @@ class Home extends Component {
                         <ContractForm contract="TutorialToken" method="transfer"
                                       labels={['To Address', 'Amount to Send']}/>
 
-                        <br/><br/>
+                        <br/><br/>*/}
                     </div>
 
-                    <div className="pure-u-1-1">
+                    {/*<div className="pure-u-1-1">
                         <h2>ComplexStorage</h2>
                         <p>Finally this contract shows data types with additional considerations. Note in the code the
                             strings below are converted from bytes to UTF-8 strings and the device data struct is
@@ -100,13 +188,13 @@ class Home extends Component {
                         <strong>Single Device Data</strong>: <ContractData contract="ComplexStorage" method="singleDD"/>
 
                         <br/><br/>
-                    </div>
+                    </div>*/}
                 </div>
             </main>
         )
     }
 }
 
-Home.contextTypes = { drizzle: PropTypes.object };
+Home.contextTypes = {drizzle: PropTypes.object};
 
 export default Home
